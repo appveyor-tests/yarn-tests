94fdbee10da489ef000bc6fc1b745837ea080ed0
Feodor Fitsner <feodor@appveyor.com>
2022-06-30 11:59:45 -0700

    Delete build.ps1

--- build.ps1
+++
@@ -1,5 +0,0 @@
-$ErrorActionPreference = "Stop"
-$expected_yarn_version = '1.16.0'
-$actual_yarn_version = (yarn --version)
-if($actual_yarn_version -ne $expected_yarn_version) { throw "Yarn version: $actual_yarn_version, expected: $expected_yarn_version"; }
-cmd /c yarn add lodash
